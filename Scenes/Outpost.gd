a2523bdd3d6ea7b0280a61c80e95c4f1267ea443
RostislavGoncharov <87071941+RostislavGoncharov@users.noreply.github.com>
2022-01-03 17:51:12 +0200

    Fades added

--- Scenes/Outpost.gd
+++ Scenes/Outpost.gd
@@ -1,17 +1,32 @@
 extends Node2D
 
-func next_stage(camera_point):
-	$Camera.set_offset(Vector2(0, camera_point.global_position.y))
+var scene_number = 0
+
+func next_stage():
+	var camera_point = $Stage3/CameraPoint3
+	
+	scene_number += 1
+	
+	match scene_number:
+		0:
+			camera_point = $Stage1/CameraPoint1
+		1: 
+			camera_point = $Stage2/CameraPoint2
+		2: 
+			camera_point = $Stage3/CameraPoint3
+		_:
+			pass
+		
+	$Camera/FadeLayer/TextureRect/AnimationPlayer.play_backwards("fade_in")
+	yield(get_tree().create_timer(1), "timeout")
+	$Camera.set_offset(Vector2(0, camera_point.global_position.y)) 
+	$Camera/FadeLayer/TextureRect/AnimationPlayer.play("fade_in")
+			
 	
 func _process(delta):
-	
-	if Input.is_action_just_pressed("ui_accept"):
-		var camera_point = $Stage3/CameraPoint3
-		next_stage(camera_point)
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().reload_current_scene()
 
 func _on_TextureButton_pressed():
-	var camera_point = $Stage2/CameraPoint2
-	next_stage(camera_point)
+	next_stage()
