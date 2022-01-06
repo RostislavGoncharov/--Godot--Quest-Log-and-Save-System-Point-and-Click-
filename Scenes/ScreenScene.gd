c3118c2eadb3b8a1abf898d5e468e3f655fabf85
RostislavGoncharov <87071941+RostislavGoncharov@users.noreply.github.com>
2022-01-06 12:38:18 +0200

    Screen scene popup added

--- Scenes/ScreenScene.gd
+++ Scenes/ScreenScene.gd
@@ -8,7 +8,23 @@ func _on_Button_pressed():
 	var video = preload("res://Assets/Video/BOV_Video_Intro.webm")
 	$Video.set_stream(video)
 	$Video.play()
+	$Button.set_deferred("visible", false)
+	$ScreenButton.set_deferred("visible", true)
 
 
 func _on_Video_finished():
 	$Video.stream = null
+
+
+func _on_ScreenButton_pressed():
+	$Popup.popup()
+	$ScreenButton.set_deferred("visible", false)
+
+
+func _on_RestartButton_pressed():
+	get_tree().change_scene("res://Scenes/Outpost.tscn")
+
+
+func _on_CancelButton_pressed():
+	$Popup.hide()
+	$ScreenButton.set_deferred("visible", true)
